673e82fed51f742c6358871b42f88dc4019b400e
GotamDahiya <apollo2562000@gmail.com>
2020-04-18 20:20:01 +0530

    Client with encryption

--- client.py
+++ client.py
@@ -1,23 +1,57 @@
 import socket
+import random
+import sys
 
-severAddrPort = ("127.0.0.1",8989)
-bufferSize = 1024
+def encrypt(msg,r_seed):
+	result = ''
+	char_in_order = [chr(x) for x in range(0,127)]
 
-localIP = "127.0.0.2"
-localPort = 8080
+	random.seed(r_seed)
+	shuffled_list = [chr(x) for x in range(0,127)]
+	random.shuffle(shuffled_list)
 
-clientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-clientSocket.bind((localIP,localPort))
+	for i in range(0, len(msg)):
+		result += shuffled_list[char_in_order.index(msg[i])]
 
-while True:
-	
-	msgFromClient = input("Enter a message")
-	bytesToSend = str.encode(msgFromClient)
-	
-	clientSocket.sendto(bytesToSend, severAddrPort)
+	return result
 
-	msgfromServer = clientSocket.recvfrom(bufferSize)
+def decrypt(msg,r_seed):
+	result=''
+	char_in_order = [chr(x) for x in range(0,127)]
 
-	msg = "Message from server {}".format(msgfromServer[0])
+	random.seed(r_seed)
+	shuffled_list = [chr(x) for x in range(0,127)]
+	random.shuffle(shuffled_list)
 
-	print(msg)
+	for i in range(0, len(msg)):
+		result += char_in_order[shuffled_list.index(msg[i])]
+
+	result += '\n'
+	return result
+
+if __name__ == '__main__':
+	severAddrPort = ("127.0.0.1",8989)
+	bufferSize = 1024
+
+	localIP = "127.0.0.2"
+	localPort = 8080
+
+	r_seed=input("Enter number for seed function-> ")
+
+	clientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+	# clientSocket.bind((localIP,localPort))
+
+	while True:
+		
+		msgFromClient = input("Enter a message-> ")
+		msgFromClient = encrypt(msgFromClient,r_seed)
+		bytesToSend = msgFromClient.encode()
+		
+		clientSocket.sendto(bytesToSend, severAddrPort)
+
+		msgFromServer = clientSocket.recvfrom(bufferSize)
+		msgFromServer = msgFromServer[0].decode()
+		msgFromServer = decrypt(msgFromServer,r_seed)
+		msg = "Message from server {}".format(msgFromServer)
+
+		print(msg)
