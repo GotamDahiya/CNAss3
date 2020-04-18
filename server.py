c1f8a986ce4ed4383d8bd4e559bb258b8fc29d63
GotamDahiya <apollo2562000@gmail.com>
2020-04-18 20:19:49 +0530

    Server with encryption

--- server.py
+++ server.py
@@ -1,28 +1,62 @@
 import socket
 import io
+import random
+import sys
 
-localIP = "127.0.0.1"
-localPort = 8989
-buffersize = 1024
+def encrypt(msg,r_seed):
+	result = ''
+	char_in_order = [chr(x) for x in range(0,127)]
 
-serverSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+	random.seed(r_seed)
+	shuffled_list = [chr(x) for x in range(0,127)]
+	random.shuffle(shuffled_list)
 
-serverSocket.bind((localIP,localPort))
+	for i in range(0, len(msg)):
+		result += shuffled_list[char_in_order.index(msg[i])]
 
-print("UDP server up and listening")
+	return result
 
-while True:
-    bytesAddrPair = serverSocket.recvfrom(buffersize)
-    message = bytesAddrPair[0]
-    address = bytesAddrPair[1]
+def decrypt(msg,r_seed):
+	result=''
+	char_in_order = [chr(x) for x in range(0,127)]
 
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP = "Client IP:{}".format(address)
+	random.seed(r_seed)
+	shuffled_list = [chr(x) for x in range(0,127)]
+	random.shuffle(shuffled_list)
 
-    print(clientMsg)
-    print(clientIP)
+	for i in range(0, len(msg)):
+		result += char_in_order[shuffled_list.index(msg[i])]
 
-    msgfromServer = input("Enter a message")
-    bytesToSend = str.encode(msgfromServer)
+    result+='\n'
+	return result
 
-    serverSocket.sendto(bytesToSend, address)
+if __name__ == '__main__':
+    localIP = "127.0.0.1"
+    localPort = 8989
+    buffersize = 1024
+
+    r_seed = input("Enter number for seed function-> ")
+
+    serverSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    serverSocket.bind((localIP,localPort))
+
+    print("UDP server up and listening")
+
+    while True:
+        bytesAddrPair = serverSocket.recvfrom(buffersize)
+        msgFromClient = bytesAddrPair[0]
+        address = bytesAddrPair[1]
+        msgFromClient = msgFromClient.decode()
+        msgFromClient = decrypt(msgFromClient,r_seed)
+        clientMsg = "Message from Client:{}".format(msgFromClient)
+        clientIP = "Client IP:{}".format(address)
+
+        print(clientMsg)
+        print(clientIP)
+
+        msgFromServer = input("Enter a message-> ")
+        msgFromServer = encrypt(msgFromServer,r_seed)
+        bytesToSend = msgFromServer.encode()
+
+        serverSocket.sendto(bytesToSend, address)
