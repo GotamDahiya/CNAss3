0b0ff7e9b11982e359081a0ef766ca923196f084
GotamDahiya <apollo2562000@gmail.com>
2020-04-20 22:44:36 +0530

    Client file

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ from udp_reliable import *
 
 
 if __name__ == '__main__':
-	severAddrPort = ("127.0.0.1",8989)
+	serverAddrPort = ("127.0.0.1",8989)
 	bufferSize = 1024
 
 	localIP = "127.0.0.2"
@@ -18,19 +18,26 @@ if __name__ == '__main__':
 	r_seed=input("Enter number for seed function(Public key)-> ")
 
 	clientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-	# clientSocket.bind((localIP,localPort))
+	clientSocket.bind((localIP,localPort))
 
 	while True:
-		
 		msgFromClient = input("Enter a message-> ")
 		msgFromClient = Encryption(msgFromClient,r_seed).encrypt()
-		bytesToSend = msgFromClient.encode()
-		
-		clientSocket.sendto(bytesToSend, severAddrPort)
-  
-		msgFromServer = clientSocket.recvfrom(bufferSize)
-		msgFromServer = msgFromServer[0].decode()
-		msgFromServer = Encryption(msgFromServer,r_seed).decrypt()
-		msg = "\nMessage from server:\n{}".format(msgFromServer)
+		data = msgFromClient.encode()
+		bytesToSend = UDPPacket(localIP,serverAddrPort[0],localPort,serverAddrPort[1],data).build()
+		clientSocket.sendto(bytesToSend, serverAddrPort)
 
-		print(msg)
+	
+		datagram,ip_addr = clientSocket.recvfrom(bufferSize)
+		packet = parse(datagram)
+		# print(packet)
+		if(checksum_receiver(datagram, packet['checksum'])):
+			msgFromServer, = packet['data']
+			msgFromServer = msgFromServer.decode()
+			msgFromServer = Encryption(msgFromServer,r_seed).decrypt()
+			print("Message from server:")
+			print(msgFromServer)
+			print("Server's IP address:")
+			print(ip_addr)
+		else:
+			print("Error! Package discarded")
